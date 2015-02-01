4d1a1bffbe48bc505f832215d8763a23b99342a2
Larry Farrell <farrelldlfarrell@gmail.com>
2015-02-01 15:41:29 -0500

    Texas downloads work.

--- texas.php
+++ texas.php
@@ -1,2 +1,144 @@
 <?php
 include 'functions.php';
+
+$reservoirs = array(
+    "abilene",
+    "addicks",
+    "alan-henry",
+    "amistad",
+    "amon-g-carter",
+    "aquilla",
+    "arlington",
+    "arrowhead",
+    "athens",
+    "austin",
+    "b-a-steinhagen",
+    "bardwell",
+    "belton",
+    "benbrook",
+    "bob-sandlin",
+    "bonham",
+    "brady-creek",
+    "bridgeport",
+    "brownwood",
+    "buchanan",
+    "caddo",
+    "canyon",
+    "cedar-creek",
+    "champion-creek",
+    "choke-canyon",
+    "cisco",
+    "coleman",
+    "coleto-creek",
+    "colorado-city",
+    "conroe",
+    "corpus-christi",
+    "crook",
+    "cypress-springs",
+    "e-v-spence",
+    "eagle-mountain",
+    "elephant-butte",
+    "falcon",
+    "fork",
+    "fort-phantom-hill",
+    "georgetown",
+    "graham",
+    "granbury",
+    "granger",
+    "grapevine",
+    "greenbelt",
+    "halbert",
+    "hords-creek",
+    "houston",
+    "houston-county",
+    "hubbard-creek",
+    "hubert-h-moss",
+    "inks",
+    "j-b-thomas",
+    "jacksonville",
+    "jim-chapman",
+    "joe-pool",
+    "kemp",
+    "kickapoo",
+    "lake-o-the-pines",
+    "lavon",
+    "leon",
+    "lewisville",
+    "limestone",
+    "livingston",
+    "lost-creek",
+    "lyndon-b-johnson",
+    "mackenzie",
+    "martin",
+    "medina",
+    "meredith",
+    "millers-creek",
+    "mineral-wells",
+    "monticello",
+    "mountain-creek",
+    "murvaul",
+    "nacogdoches",
+    "natural-dam",
+    "navarro-mills",
+    "new-terrell-city",
+    "nocona",
+    "north-fork-buffalo-creek",
+    "o-c-fisher",
+    "o-h-ivie",
+    "oak-creek",
+    "palestine",
+    "palo-duro",
+    "palo-pinto",
+    "pat-cleburne",
+    "pat-mayse",
+    "possum-kingdom",
+    "proctor",
+    "ray-hubbard",
+    "ray-roberts",
+    "red-bluff",
+    "richland-chambers",
+    "sam-rayburn",
+    "somerville",
+    "squaw-creek",
+    "stamford",
+    "stillhouse-hollow",
+    "sulphur-springs",
+    "sweetwater",
+    "tawakoni",
+    "texana",
+    "texoma",
+    "toledo-bend",
+    "travis",
+    "twin-buttes",
+    "tyler",
+    "waco",
+    "waxahachie",
+    "weatherford",
+    "white-river",
+    "whitney",
+    "worth",
+    "wright-patman"
+);
+
+foreach($reservoirs as $reservoir) {
+    $path = "http://waterdatafortexas.org/reservoirs/individual/" . $reservoir . "-30day.csv";
+    $file_name = 'raw_data/tx/' . $reservoir . ".csv";
+    $action = (file_exists($file_name)) ? "a" : "wb";
+    get_records($path, $file_name, $action);
+   // echo $reservoir . " downloaded\n";
+
+    // Clean up the data
+    $fh = fopen('data/tx/'. $reservoir . ".csv", $action);
+    if (($handle = fopen($file_name, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            if(preg_match('/$\d+/', $data[0])) {
+                fputcsv($fh, array());
+            } else {
+                continue;
+            }
+
+        }
+        fclose($handle);
+    }
+    fclose($fh);
+}
