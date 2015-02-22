4803f957856d2d67a439e358e0cf8a561e4e99f4
Larry Farrell <farrelldlfarrell@gmail.com>
2015-02-22 11:46:15 -0500

    Move routes and controllers into 1 file

---
+++ js/routes-controllers.js
@@ -0,0 +1,41 @@
+"use strict";
+
+angular.module('westernWaterApp', ["ngRoute", "ui.bootstrap"]);
+
+angular.module('westernWaterApp').config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/main.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: 'partials/about.html',
+            controller: 'aboutController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+angular.module('westernWaterApp').controller('aboutController', ['$scope', function($scope) {
+}]);
+
+angular.module('westernWaterApp').controller('mainController', ['$scope', function($scope) {
+    d3.json('js/maps/contig_us.geo.json', function(map_data) {
+        $scope.map_data = map_data;
+
+        d3.csv('station_data/all_resv.csv', function(stations) {
+            $scope.stations = stations;
+            $scope.$apply();
+        });
+
+        d3.csv('data/all.csv', function(data) {
+            $scope.data = data;
+            $scope.graphloading = false;
+            $scope.graphloaded = true;
+            $scope.$apply();
+        });
+
+        $scope.$apply();
+    });
+}]);
