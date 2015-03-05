9470ba34953ea82f41d3003631de40b43c9eb41d
Larry Farrell <farrelldlfarrell@gmail.com>
2015-03-04 20:43:14 -0500

    Clear out current month on errors

---
+++ cal_fix.php
@@ -0,0 +1,19 @@
+<?php
+$reservoirs = scandir('data/ca_month');
+
+foreach($reservoirs as $reservoir) {
+    if(!preg_match('/^\./', $reservoir)) {
+        $ft = fopen('data/ca/' . $reservoir, "wb");
+        if (($handle = fopen('data/ca_month/' . $reservoir, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if(!preg_match('/02\/2015/', $data[4])) {
+                    fputcsv($ft, $data);
+                }
+
+            }
+        }
+        fclose($handle);
+        fclose($ft);
+    }
+
+}
