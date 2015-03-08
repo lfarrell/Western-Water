cffecd56c33f7433c92d7ff23173bae3eca5de74
Larry Farrell <farrelldlfarrell@gmail.com>
2015-03-08 15:42:59 -0400

    Add in basic individual state files

--- js/routes-controllers.js
+++ js/routes-controllers.js
@@ -8,6 +8,38 @@ angular.module('westernWaterApp').config(function($routeProvider) {
             templateUrl: 'partials/main.html',
             controller: 'mainController'
         })
+        .when('/arizona', {
+            templateUrl: 'partials/arizona.html',
+            controller: 'arizonaController'
+        })
+        .when('/california', {
+            templateUrl: 'partials/california.html',
+            controller: 'californiaController'
+        })
+        .when('/colorado', {
+            templateUrl: 'partials/colorado.html',
+            controller: 'coloradoController'
+        })
+        .when('/nevada', {
+            templateUrl: 'partials/nevada.html',
+            controller: 'nevadaController'
+        })
+        .when('/new_mexico', {
+            templateUrl: 'partials/new_mexico.html',
+            controller: 'newmexicoController'
+        })
+        .when('/texas', {
+            templateUrl: 'partials/texas.html',
+            controller: 'texasController'
+        })
+        .when('/utah', {
+            templateUrl: 'partials/utah.html',
+            controller: 'utahController'
+        })
+        .when('/wyoming', {
+            templateUrl: 'partials/wyoming.html',
+            controller: 'wyomingController'
+        })
         .when('/about', {
             templateUrl: 'partials/about.html',
             controller: 'aboutController'
@@ -41,4 +73,44 @@ angular.module('westernWaterApp').controller('mainController', ['$scope', functi
 
         $scope.$apply();
     });
+}]);
+
+angular.module('westernWaterApp').controller('arizonaController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('californiaController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('coloradoController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('nevadaController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('newmexicoController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('texasController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('utahController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
+}]);
+
+angular.module('westernWaterApp').controller('wyomingController', ['$scope', function($scope) {
+    $scope.graphloading = false;
+    $scope.graphloaded = true;
 }]);
