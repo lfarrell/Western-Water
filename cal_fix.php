c86a3d2ef13bdf63aa65cd2a532cb2ec2c5911f0
lfarrell <farrelldlfarrell@gmail.com>
2015-05-16 15:48:07 -0400

    comment out figures unavailable. CA fully done

--- cal_fix.php
+++ cal_fix.php
@@ -3,10 +3,10 @@ $reservoirs = scandir('data/ca_month');
 
 foreach($reservoirs as $reservoir) {
     if(!preg_match('/^\./', $reservoir)) {
-        $ft = fopen('data/uc/' . $reservoir, "wb");
+        $ft = fopen('data/ca/' . $reservoir, "wb");
         if (($handle = fopen('data/ca_month/' . $reservoir, "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                if(!preg_match('/03\/2015/', $data[4])) {
+                if(!preg_match('/04\/2015/', $data[4])) {
                     fputcsv($ft, $data);
                 }
 
