80c937a6a27428627f730114fed854476025210a
lfarrell <farrelldlfarrell@gmail.com>
2015-06-22 20:15:51 -0400

    Get rid of debug statement & CA note.

--- js/services.js
+++ js/services.js
@@ -773,7 +773,7 @@ angular.module('westernWaterApp').service('chartService', function() {
 
         stations.forEach(function(d) {
             var res_total = _.last(sorted[d.reservoir]); //console.log(res_total)
-            if(res_total === undefined) console.log(d.reservoir)
+      //      if(res_total === undefined) console.log(d.reservoir)
             d.pct_capacity = (res_total !== undefined) ? res_total.pct_capacity : undefined;
             d.capacity = (res_total !== undefined) ? res_total.capacity : undefined;
         });
