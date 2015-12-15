6c8ec71c538e2d3a54b3797231c798c59a26e005
lfarrell <farrelldlfarrell@gmail.com>
2015-12-14 20:36:21 -0500

    follow redirects on curl requests

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function get_records($path, $filename, $action) {
 
     curl_setopt($ch, CURLOPT_FILE, $fp);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
   //  curl_exec($ch);
     if(curl_exec($ch) === false)
